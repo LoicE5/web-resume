e0374015478b24d9ffde26889a65f299cf9f5925
Loïc <59501884+LoicE5@users.noreply.github.com>
2020-12-24 09:49:36 +0100

    Update connect.php

--- assets/connect.php
+++ assets/connect.php
@@ -1,9 +1,9 @@
 <?php
     # mysqli simple connection system
-    $user = 'letien04';
-    $pass = '5Wiyqaeh5j';
-    $db = 'letien04_db';
-    $server = 'sqletud.u-pem.fr';
+    $user = 'anid';
+    $pass = 'apassword';
+    $db = 'adb';
+    $server = 'aserver';
 
     $user2 = 'root';
     $pass2 = 'root';
