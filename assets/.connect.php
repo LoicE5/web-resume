08f91108aef9c92dc2dea84d6481feb39c6cfa53
Loïc <s5g@icloud.com>
2020-12-24 10:01:51 +0100

    trying .connect.php

--- assets/.connect.php
+++ assets/.connect.php
@@ -1,9 +1,9 @@
 <?php
-    # mysqli simple connection system //
-    $user = 'anid';
-    $pass = 'apassword';
-    $db = 'adb';
-    $server = 'aserver';
+    # mysqli simple connection system
+    $user = 'letien04';
+    $pass = '5Wiyqaeh5j';
+    $db = 'letien04_db';
+    $server = 'sqletud.u-pem.fr';
 
     $user2 = 'root';
     $pass2 = 'root';
